af0ee999f8abc019c9e5ebc7ca0147ab8bd75c15
Gauri9 <gauriwalke99@gmail.com>
2021-09-15 16:44:55 +0530

    commit2

--- GitHubReposJavaProject/src/com/lti/demo/Product.java
+++ GitHubReposJavaProject/src/com/lti/demo/Product.java
@@ -1,5 +1,7 @@
 package com.lti.demo;
 
 public class Product {
-
+	public static void main() {
+		System.out.println("UserApp");
+	}
 }
--- GitHubReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -1,5 +1,7 @@
 package com.lti.demo;
 
 public class UserApp {
-
+	public static void main() {
+		System.out.println("UserApp");
+	}
 }
